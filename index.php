50b7daa7e63f181e8053a0d0f6d62dc8e2e34961
leva <levka181@gmail.com>
2025-12-19 19:17:42 +0300

    ПровоторовЛев2

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 // require_once __DIR__ . '/../pizza-library/src/Pizza.php';
 // require_once __DIR__ . '/../pizza-library/src/PizzaStore.php';
 
-require_once __DIR__ . '/src/CheesePizza.php';
-require_once __DIR__ . '/src/PepperoniPizza.php';
-require_once __DIR__ . '/src/VeggiePizza.php';
-require_once __DIR__ . '/src/MyPizzaStore.php';
+// require_once __DIR__ . '/src/CheesePizza.php';
+// require_once __DIR__ . '/src/PepperoniPizza.php';
+// require_once __DIR__ . '/src/VeggiePizza.php';
+// require_once __DIR__ . '/src/MyPizzaStore.php';
+
+
+require __DIR__ . '/../vendor/autoload.php';
 
 use PizzaApp\MyPizzaStore;
 
